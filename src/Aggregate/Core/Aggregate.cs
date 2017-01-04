c46dab6bb1cce0f999ca2af743a952f7f5a8c549
Ricardo Bartolomeu <r3dbag@gmail.com>
2017-01-04 08:44:02 +0000

    Cpu Code.

--- src/Aggregate/Core/Aggregate.cs
+++ src/Aggregate/Core/Aggregate.cs
@@ -1,167 +1,54 @@
 using System;
-using System.Drawing;
+using System.Linq;
 using System.Threading.Tasks;
-using Alea;
-using Alea.CSharp;
-using Alea.Parallel;
 
-namespace Mandelbrot
+namespace Aggregate
 {
-    internal static class Mandelbrot
+    internal static class Aggregate
     {
-        // CPU: Using Native GDI+ Bitmap!
-        internal static Image RenderCpu1(Bounds bounds)
+        // CPU: (Sum) Using Native Sequential Loop!
+        internal static long ComputeCpu1(int[] array)
         {
-            var result = new FastBitmap(bounds.ViewportWidth, bounds.ViewportHeight);
-            bounds.AdjustAspectRatio();
-            var scale = (bounds.XMax - bounds.XMin) / bounds.ViewportWidth;
+            var result = 0L;
 
-            Parallel.For(0, bounds.ViewportHeight, y =>
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            for (int i = 0; i < array.Length; i++)
             {
-                for (var x = 0; x < bounds.ViewportWidth; ++x)
-                {
-                    var c = new Complex(bounds.XMin + x * scale, bounds.YMin + y * scale);
-                    var z = c;
-
-                    for (byte i = 0; i < 255; ++i)
-                    {
-                        z = z * z + c;
-
-                        if (z.Magnitude() >= 2)
-                        {
-                            result.SetPixel(x, y, Color.FromArgb(i, i, i));
-                            break;
-                        }
-                    }
-                }
-            });
-
-            return result.Bitmap;
-        }
-
-        // CPU: Using byte Array!
-        internal static Image RenderCpu2(Bounds bounds)
-        {
-            var result = new byte[3 * bounds.ViewportWidth * bounds.ViewportHeight];
-            bounds.AdjustAspectRatio();
-            var scale = (bounds.XMax - bounds.XMin) / bounds.ViewportWidth;
-
-            Parallel.For(0, bounds.ViewportHeight, y =>
-            {
-                for (var x = 0; x < bounds.ViewportWidth; ++x)
-                {
-                    var offset = 3 * (y * bounds.ViewportWidth + x);
-                    var c = new Complex(bounds.XMin + x * scale, bounds.YMin + y * scale);
-
-                    ComputeMandelbrotAtOffset(result, c, offset);
-                }
-            });
-
-            return FastBitmap.FromByteArray(result, bounds.ViewportWidth, bounds.ViewportHeight);
-        }
-
-        // GPU: Using byte Array!
-        internal static Image RenderGpu1(Bounds bounds)
-        {
-            var result = new byte[3 * bounds.ViewportWidth * bounds.ViewportHeight];
-            bounds.AdjustAspectRatio();
-            var lp = ComputeLaunchParameters(bounds);
-            var scale = (bounds.XMax - bounds.XMin) / bounds.ViewportWidth;
-
-            Gpu.Default.Launch(() =>
-            {
-                var x = blockDim.x * blockIdx.x + threadIdx.x;
-                var y = blockDim.y * blockIdx.y + threadIdx.y;
-                var offset = 3 * (y * bounds.ViewportWidth + x);
-                
-                var c = new Complex
-                {
-                    Real      = bounds.XMin + x * scale,
-                    Imaginary = bounds.YMin + y * scale,
-                };
-
-                ComputeMandelbrotAtOffset(result, c, offset);
-            }, lp);
-
-            return FastBitmap.FromByteArray(result, bounds.ViewportWidth, bounds.ViewportHeight);
-        }
-        
-        // GPU: Allocating Memory on GPU only!
-        internal static Image RenderGpu2(Bounds bounds)
-        {
-            var deviceResult = Gpu.Default.Allocate<byte>(3 * bounds.ViewportWidth * bounds.ViewportHeight);
-            bounds.AdjustAspectRatio();
-            var lp = ComputeLaunchParameters(bounds);
-            var scale = (bounds.XMax - bounds.XMin) / bounds.ViewportWidth;
-
-            Gpu.Default.Launch(() =>
-            {
-                var x = blockDim.x * blockIdx.x + threadIdx.x;
-                var y = blockDim.y * blockIdx.y + threadIdx.y;
-                var offset = 3 * (y * bounds.ViewportWidth + x);
-
-                var c = new Complex
-                {
-                    Real      = bounds.XMin + x * scale,
-                    Imaginary = bounds.YMin + y * scale,
-                };
-
-                ComputeMandelbrotAtOffset(deviceResult, c, offset);
-            }, lp);
-
-            return FastBitmap.FromByteArray(Gpu.CopyToHost(deviceResult), bounds.ViewportWidth, bounds.ViewportHeight);
-        }
-
-        // GPU: Parallel.For!
-        internal static Image RenderGpu3(Bounds bounds)
-        {
-            var result = new byte[3 * bounds.ViewportWidth * bounds.ViewportHeight];
-            bounds.AdjustAspectRatio();
-            var scale = (bounds.XMax - bounds.XMin) / bounds.ViewportWidth;
-
-            Gpu.Default.For(0, bounds.ViewportWidth * bounds.ViewportHeight, i =>
-            {
-                var x = i % bounds.ViewportWidth;
-                var y = i / bounds.ViewportWidth;
-                var offset = 3 * i;
-
-                var c = new Complex
-                {
-                    Real      = bounds.XMin + x * scale,
-                    Imaginary = bounds.YMin + y * scale,
-                };
-
-                ComputeMandelbrotAtOffset(result, c, offset);
-            });
-
-            return FastBitmap.FromByteArray(result, bounds.ViewportWidth, bounds.ViewportHeight);
-        }
-
-        // ReSharper disable once SuggestBaseTypeForParameter
-        private static void ComputeMandelbrotAtOffset(byte[] result, Complex c, int offset)
-        {
-            var z = c;
-
-            for (byte i = 0; i < 255; ++i)
-            {
-                z = z * z + c;
-
-                if (z.Magnitude() >= 2)
-                {
-                    result[offset + 0] = i;
-                    result[offset + 1] = i;
-                    result[offset + 2] = i;
-                    break;
-                }
+                result += array[i];
             }
+
+            return result;
         }
 
-        private static LaunchParam ComputeLaunchParameters(Bounds bounds)
+        // CPU: (Sum) Using Parallel.ForEach!
+        internal static long ComputeCpu2(int[] array)
         {
-            const int tpb = 32;
-            var width = bounds.ViewportWidth;
-            var height = bounds.ViewportHeight;
-            return new LaunchParam(new dim3((width + (tpb - 1)) / tpb, (height + (tpb - 1)) / tpb), new dim3(tpb, tpb));
+            var result = 0L;
+            var locker  = new object();
+
+            Parallel.ForEach(array, () => 0L, (value, state, local) => local + value, x =>
+            {
+                lock (locker)
+                {
+                    result += x;
+                }
+            });
+
+            return result;
         }
+
+        // CPU: (Sum) Using Linq Aggregate!
+        internal static long ComputeCpu3(int[] array)
+        {
+            return array.Aggregate(0L, (a, b) => a + b);
+        }
+
+        // CPU: (Sum) Using Linq Aggregate in Parallel!
+        internal static long ComputeCpu4(int[] array)
+        {
+            return array.AsParallel().Aggregate(0L, (a, b) => a + b);
+        }
+
+        // Todo: Proper CPU-P without locks!
     }
 }
--- src/Aggregate/Program.cs
+++ src/Aggregate/Program.cs
@@ -1,79 +1,51 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Imaging;
+using System.Linq;
 
-namespace Mandelbrot
+namespace Aggregate
 {
     internal static class Program
     {
         private static void Main()
         {
-            const int scale  = 4;
-            const int width  = scale * 1920;
-            const int height = scale * 960;
+            const int length = 260000000;
+            var data = Enumerable.Range(1, length).ToArray();
 
-            var bounds1 = new Bounds
-            {
-                ViewportWidth  = width,
-                ViewportHeight = height,
-                XMin = -2.2f,
-                XMax = +1.0f,
-                YMin = -1.0f,
-                YMax = +1.0f,
-            };
+            Measure(() => Aggregate.ComputeCpu1(data), "CPU: (Sum) Using Native Sequential Loop!");
+            Measure(() => Aggregate.ComputeCpu2(data), "CPU: (Sum) Using Parallel.ForEach!");
+            Measure(() => Aggregate.ComputeCpu3(data), "CPU: (Sum) Using Linq Aggregate!");
+            Measure(() => Aggregate.ComputeCpu4(data), "CPU: (Sum) Using Linq Aggregate in Parallel!");
 
-            var bounds2 = new Bounds
-            {
-                ViewportWidth  = width,
-                ViewportHeight = height,
-                XMin = -1.0f,
-                XMax = +1.0f,
-                YMin = -1.0f,
-                YMax = +1.0f,
-            };
-
-            Measure(() => Mandelbrot.RenderCpu1(bounds1), "mandelbrot.cpu.1.png", "CPU: (Mandelbrot) Using Native GDI+ Bitmap!");
-            Measure(() => Mandelbrot.RenderCpu2(bounds1), "mandelbrot.cpu.2.png", "CPU: (Mandelbrot) Using byte Array!");
-            Measure(() => Mandelbrot.RenderGpu1(bounds1), "mandelbrot.gpu.1.png", "GPU: (Mandelbrot) Using byte Array!");
-            Measure(() => Mandelbrot.RenderGpu2(bounds1), "mandelbrot.gpu.2.png", "GPU: (Mandelbrot) Allocating Memory on GPU only!");
-            Measure(() => Mandelbrot.RenderGpu3(bounds1), "mandelbrot.gpu.3.png", "GPU: (Mandelbrot) Parallel.For!");
-
-            Measure(() => Julia.RenderCpu1(bounds2), "julia.cpu.1.png", "CPU: (Julia) Using Native GDI+ Bitmap!");
-            Measure(() => Julia.RenderCpu2(bounds2), "julia.cpu.2.png", "CPU: (Julia) Using byte Array!");
-            Measure(() => Julia.RenderGpu1(bounds2), "julia.gpu.1.png", "GPU: (Julia) Using byte Array!");
-            Measure(() => Julia.RenderGpu2(bounds2), "julia.gpu.2.png", "GPU: (Julia) Allocating Memory on GPU only!");
-            Measure(() => Julia.RenderGpu3(bounds2), "julia.gpu.3.png", "GPU: (Julia) Parallel.For!");
+            Console.WriteLine("Expected: {0:D}", length * ((long)length + 1) / 2);
 
             Console.WriteLine("Done!");
             Console.ReadLine();
         }
 
-        private static void Measure(Func<Image> func, string fileName, string description)
+        private static void Measure(Func<long> func, string description)
         {
             Func<Stopwatch, string> formatElapsedTime = (watch) => watch.Elapsed.TotalSeconds >= 1
                 ? $"{watch.Elapsed.TotalSeconds}s"
                 : $"{watch.ElapsedMilliseconds}ms";
 
             var sw1 = Stopwatch.StartNew();
-            var bmp1 = func();
+            var result1 = func();
             sw1.Stop();
 
             Console.WriteLine(new string('-', 43));
             Console.WriteLine(description);
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("{0} [Cold]", formatElapsedTime(sw1));
+            Console.WriteLine("{0} - {1} [Cold]", result1, formatElapsedTime(sw1));
             Console.ResetColor();
-            bmp1.Save(fileName, ImageFormat.Png);
 
             Console.WriteLine();
 
             var sw2 = Stopwatch.StartNew();
-            func();
+            var result2 = func();
             sw2.Stop();
             Console.WriteLine(description);
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("{0} [Warm]", formatElapsedTime(sw2));
+            Console.WriteLine("{0} - {1} [Cold]", result2, formatElapsedTime(sw2));
             Console.ResetColor();
 
             Console.WriteLine(new string('-', 43));
