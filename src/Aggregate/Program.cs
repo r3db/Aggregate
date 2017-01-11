8dd41fff46100bd7bb23e1ef97be797a781c6f42
Ricardo Bartolomeu <r3dbag@gmail.com>
2017-01-11 03:41:31 +0000

    .

--- src/Aggregate/Program.cs
+++ src/Aggregate/Program.cs
@@ -10,7 +10,6 @@ namespace Aggregate
         private static void Main()
         {
             //const int length = 260000023;
-            //const int length = 82000015;
             const int length = 82000015;
             var data = Enumerable.Range(1, length).Select(x => x % 8).ToArray();
             var expected = data.Sum();
